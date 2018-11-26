bf41f72b91e5f0f00f749a2de3be7c181e4d9e5f
Abdulkadir Cicik <42738040+acicik@users.noreply.github.com>
2018-11-26 17:48:22 +0300

    Our first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+
+// core module -- 
+// file or folder -- ./underscore  -- underscore.js or underscore/index.js
+// node_modules -- 
+
+var res = _.contains([1, 2, 3], 3);
+
+console.log(res);
